59444ca17f9641f9d1d61d49f70f76449c9cf696
Chris Ortman <chrisortman@gmail.com>
2017-09-19 13:42:29 +0000

    Refactored to use variables

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -5,23 +5,17 @@
 # and performs no computations.
 # It will display the name and contact information for a teacher.
 
-# display the full name
-print("Chris Ortman")
+name = "Chris Ortman"
+address = "742 Evergreen Terrace"
+city_state_zip = "Springfield, SA, 55555"
+phone = "555-867-5309"
+area = "CIS-121"
 
-# display the address
-print("742 Evergreen Terrace")
-
-
-
-
-# display the city, state and zip
-print("Springfield, SA, 55555")
-
-# display the phone number
-print("555-867-5309")
-
-# display the course area
-print("CIS-121")
+print(name)
+print(address)
+print(citystatezip)
+print(phone)
+print(area)
 
 
 
