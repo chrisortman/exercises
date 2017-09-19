5600dee6e3ca591f126c26ad3c53c82d0d7e0abd
Chris Ortman <chrisortman@gmail.com>
2017-09-19 13:17:40 +0000

    Address program

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,18 +6,27 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-
+print("Chris Ortman")
 
 # display the address
+print("742 Evergreen Terrace")
+
+
 
 
 # display the city, state and zip
-
+print("Springfield, SA, 55555")
 
 # display the phone number
-
+print("555-867-5309")
 
 # display the course area
+print("CIS-121")
+
+
+
+
+
 
 
 
