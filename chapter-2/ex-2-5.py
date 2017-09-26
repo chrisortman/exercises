5f21a0ae7d42b8eeb26210c44244624d98da801c
Chris Ortman <chris-ortman@uiowa.edu>
2017-09-25 21:42:37 -0500

    Exercise 5 & 6

--- chapter-2/ex-2-5.py
+++ chapter-2/ex-2-5.py
@@ -7,18 +7,24 @@
 
 # Variables to hold the three distances.
 # be sure to initialize these as floats.
-
+distance_six_hours = 0.0
+distance_ten_hours = 0.0
+distance_fifteen_hours = 0.0
 
 # Constant for the speed.
-
+SPEED = 55
 
 # Calculate the distance the car will travel in
 # 6, 10, and 15 hours.
-
+distance_six_hours = 6 * SPEED
+distance_ten_hours = 10 * SPEED
+distance_fifteen_hours = 15 * SPEED
 
 
 # Print the results for all calculations.
-
+print("After 6 hours you will have traveled", distance_six_hours, "miles")
+print("After 10 hours you will have traveled", distance_ten_hours, "miles")
+print("After 15 hours you will have traveled", distance_fifteen_hours, "miles")
 
 
 
--- chapter-2/ex-2-6.py
+++ chapter-2/ex-2-6.py
@@ -7,28 +7,39 @@
 
 # Variables for purchase amount, state tax, county tax, total tax and total sale
 # all initialized as floats
-
+purchase_amount = 0.0
+state_tax = 0.0
+county_tax = 0.0
+total_tax = 0.0
+total_sale = 0.0
 
 # Constants for the state and county tax rates
-
+COUNTY_TAX_RATE = 0.06
+STATE_TAX_RATE = 0.07
 
 # Get the amount of purchase from the user, casting it to a float.
-
+purchase_amount = input("Enter the total purchase amount: ")
+purchase_amount = float(purchase_amount)
 
 # Calculate the state sales tax.
-
+state_tax = purchase_amount * STATE_TAX_RATE
 
 # Calculate the county sales tax.
-
+county_tax = purchase_amount * COUNTY_TAX_RATE
 
 # Sum the total tax.
-
+total_tax = state_tax + county_tax
 
 # Calculate the total of the sale.
-
+total_sale = purchase_amount + total_tax
 
 # Print detailed information about the sale, formatting all values to two decimal places.
-
+line = "{:<15} {:>10.2f}"
+print(line.format("Purchase:",purchase_amount))
+print(line.format("State Tax:",state_tax))
+print(line.format("County Tax:",county_tax))
+print(line.format("Total Tax:",total_tax))
+print(line.format("Total:",total_sale))
 
 
 
