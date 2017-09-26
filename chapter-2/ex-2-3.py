1e3c21d246ebe0755a169399feff48bbe6fd2c9b
Chris Ortman <chris-ortman@uiowa.edu>
2017-09-25 21:10:59 -0500

    Exercise 3

--- chapter-2/ex-2-3.py
+++ chapter-2/ex-2-3.py
@@ -7,21 +7,23 @@
 
 # Variables to hold the size of the tract and number of acres.
 # be sure to initialize these as floats
-
+tract_size_feet = 0.0
+number_of_acres = 0.0
 
 # Constant for the number of square feet in an acre.
-
+SQ_FEET_TO_ACRE = 43560
 
 # Get the square feet in the tract from the user.
 # you will need to convert this input to a float
-
+tract_size_feet = input("Enter size of tract in square feet: ")
+tract_size_feet = float(tract_size_feet)
 
 # Calculate the number of acres.
-
+number_of_acres = tract_size_feet / SQ_FEET_TO_ACRE
 
 # Print the number of acres.
 # remember to format the acres to two decimal places
-
+print("There are ",format(number_of_acres,".2f")," acres", sep="")
 
 
 
