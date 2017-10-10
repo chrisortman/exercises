8793a693861d9b502c8f30684106d5fb1c768a96
Chris Ortman <chris-ortman@uiowa.edu>
2017-10-09 22:28:05 -0500

    Some chapter 3 exercises

--- chapter-3/exercise-3-2.py
+++ chapter-3/exercise-3-2.py
@@ -7,29 +7,48 @@
 
 # Local variables
 # you need length, width and area for A and for B
+length_a = 0.0
+width_a = 0.0
+area_a = 0.0
+
+length_b = 0.0
+width_b = 0.0
+area_b = 0.0
 # initialize these as floats
 
 
 # Get length A from the user and convert it to a float
-
+length_a = float(input("Enter length for rectangle A:"))
 
 # Get width A from the user and convert it to a float
+width_a = float(input("Enter width for rectangle A:"))
 
 
 # Get length B from the user and convert it to a float
+length_b = float(input("Enter length for rectangle B:"))
 
 
 # Get width B from the user and convert it to a float
+width_b = float(input("Enter width for rectangle B:"))
 
 
 # Calculate area A
-
+area_a = length_a * width_a
 
 # Calculate area B
+area_b = length_b * width_b
 
 
 # Print each area, formatting the values to 2 decimal places
+print("Area A: ", format(area_a, '.2f'))
+print("Area B: ", format(area_b, '.2f'))
 
+if area_a > area_b:
+    print("A is greater")
+elif area_b > area_a:
+    print("B is greater")
+else:
+    print("A and B are equal")
 # if area A is greater, print "A is greater" message.
 
 # else if area B is greater, print "B is greater" message.
--- chapter-3/exercise-3-3.py
+++ chapter-3/exercise-3-3.py
@@ -7,19 +7,29 @@
 
 # Variables to hold an age and a category.
 # initialize age as an int and category as a string.
+age = 0
+category = ""
 
 # Get the person's age.
 # remember to convert the input to an int.
-
+age = int(input("Enter your age in years plz:"))
 
 # Determine if the person is an infant, child, teenager, or adult and set the category.
 # Use a series of if ... elif ... etc. statements.
+if age >= 25:
+    category = "Adult"
+elif age >= 13:
+    category = "Teenager"
+elif age >= 3:
+    category = "Child"
+else:
+    category = "Infant"
 
 
 
 
 
 # display a message with the age category.
-
+print("You are a(n)", category)
 
 
