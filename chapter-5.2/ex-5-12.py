8b3132ee0c2acd0988f18ed2580cb7fbf345659f
Chris Ortman <chris-ortman@uiowa.edu>
2017-10-16 22:25:02 -0500

    5-12

--- chapter-5.2/ex-5-12.py
+++ chapter-5.2/ex-5-12.py
@@ -8,33 +8,42 @@
 
 
 # define the main function
-
+def main():
     # Define local variables to hold two integers
 
 
     # prompt the user for the first integer
-    
+    num1 = prompt_user_for("first")
     
     # prompt the user for the second integer
+    num2 = prompt_user_for("second")
 
 
     # print the return value from calling a function to find the greater of two integers
     # the two integers are passed as arguments
+    bigger_num = find_greater(num1,num2)
+    display_bigger_num(bigger_num)
 
- 
+def prompt_user_for(something):
+    return int(input("Enter " + something + " number: "))
+
 # Define a function to compare integer values.
 # This function accepts two integer parameters,
 # compares them,
 # and returns the value of the greater.
+def find_greater(x,y):
+    if x > y:
+        return x
+    elif y > x:
+        return y
+    else:
+        return x
 
-	# if the first integer is greater, return the first integer
-
-	# else, return the second integer
-
-
+def display_bigger_num(num):
+    print("The bigger number is: ",num)
 
 # Call the main function to start the program
-
+main()
 
 
 
