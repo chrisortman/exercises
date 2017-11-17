050a0866f3ca86a1d5032aa45a12ee5e32f44a43
Chris Ortman <chris-ortman@uiowa.edu>
2017-11-16 19:37:50 -0600

    Ch7 Ex1

--- chapter-7/ex_7_1.py
+++ chapter-7/ex_7_1.py
@@ -7,25 +7,47 @@
 
 
 # Define the main function
-
+def main():
     # Define local float variable for rtotal sales
 
 
     # Initialize lists for daily sales and days of the week (7 elements each)
+    daily_sales = [0.0 for i in range(7)]
+    days_of_week = ["Sunday",
+                    "Monday",
+                    "Tuesday",
+                    "Wednesday",
+                    "Thursday",
+                    "Friday",
+                    "Saturday"]
 
     # loop 7 times
-
-        # get the daily sales for the current day by name
-        # use a function to validate the input can be cast to a float
-
-        # add the daily sales to the total
+    for i in range(7):
+        need_value = True
+        prompt_message = "Enter sales for {}:".format(days_of_week[i])
+        while need_value:
+            # get the daily sales for the current day by name
+            day_total = input(prompt_message)
+            # use a function to validate the input can be cast to a float
+            if can_cast_to_float(day_total):
+                # add the daily sales to the total
+                daily_sales[i] = float(day_total)
+                need_value = False
+            else:
+                print("That's not a number")
 
 
     # Display total sales; use a $ and format the value to two decimal places
-
+    print("Total Sales: ${:.2f}".format(sum(daily_sales)))
 
 
 # Include a function to ensure the program has valid float values
+def can_cast_to_float(x):
+    try:
+        float(x)
+        return True
+    except:
+        return False
 
 
 
